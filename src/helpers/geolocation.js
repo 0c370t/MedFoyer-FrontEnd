5f124d5a70d16761c9b702f77cee418482b30b70
TechSolomon <2001solomon@gmail.com>
2020-04-25 12:48:11 -0800

    Updated error text messages

--- src/helpers/geolocation.js
+++ src/helpers/geolocation.js
@@ -1,13 +1,13 @@
 export function getErrorCode(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            return "MedFoyer cannot function if you block GPS";
+            return "MedFoyer cannot function if you block GPS.";
         case error.POSITION_UNAVAILABLE:
             return "We are unable to determine your location.";
         case error.TIMEOUT:
-            return "Getting your location took too long, please try again!.";
+            return "Getting your location took too long, please try again!";
         case error.UNKNOWN_ERROR:
-            return "An unknown error occurred.";
+            return "An unknown error occurred. Please contact MedFoyer support.";
     }
 }
 
