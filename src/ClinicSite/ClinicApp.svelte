c216b6cdeb3e9db5b1b256f6b44c24d07ae76ccd
Brian Donald <contact@noimbrian.com>
2020-06-30 12:46:17 -0500

    Big authentication changes.
    - Add email verification flow
    - Add password forgotten flow
    - Prompt user for email verification on each login if they have not already verified

--- src/ClinicSite/ClinicApp.svelte
+++ src/ClinicSite/ClinicApp.svelte
@@ -1,34 +1,39 @@
 <script>
-    import {createClient} from "../API/init";
-    import {setClient} from "svelte-apollo";
-    import {Router, Route} from "svelte-routing";
+    import {createClient} from '../API/init';
+    import {setClient} from 'svelte-apollo';
+    import {Router, Route} from 'svelte-routing';
     import AppointmentDashboard from './pages/AppointmentDashboard.svelte';
     import PatientOverview from './pages/PatientOverview.svelte';
-    import Login from './pages/Login.svelte';
-    import CompleteProfile from './pages/CompleteProfile.svelte'
-    import {CheckSession} from "../helpers/auth/ClinicAuthentication";
-    import Redirect from "../Components/Routing/Redirect.svelte";
-    import ClinicWrapper from "./pages/ClinicWrapper.svelte";
-    import AdminOverview from "./pages/AdminOverview.svelte";
-    import AppointmentOverview from "./pages/AppointmentOverview.svelte";
-    export let url = "";
+    import Login from './pages/Login/Login.svelte';
+    import CompleteProfile from './pages/Login/CompleteProfile.svelte'
+    import VerifyEmail from './pages/Login/VerifyEmail.svelte';
+    import ChangePassword from './pages/Login/ChangePassword.svelte';
+    import {CheckSession} from '../helpers/auth/ClinicAuthentication';
+    import Redirect from '../Components/Routing/Redirect.svelte';
+    import ClinicWrapper from './pages/ClinicWrapper.svelte';
+    import AdminOverview from './pages/AdminOverview.svelte';
+    import AppointmentOverview from './pages/AppointmentOverview.svelte';
+    
+    export let url = '';
 
     const client = createClient();
     setClient(client);
 
-    if (!window.location.pathname.includes("login") && !window.location.pathname.includes("patient")) {
+    if (!window.location.pathname.includes('login') && !window.location.pathname.includes('patient')) {
         CheckSession();
     }
 </script>
 
 <Router {url}>
-    <Route path="/clinic"><ClinicWrapper component="{AppointmentDashboard}"/></Route>
-    <Route path="/clinic/appointments"><ClinicWrapper component="{AppointmentOverview}"/></Route>
-    <Route path="/clinic/patients"><ClinicWrapper component="{PatientOverview}"/></Route>
-    <Route path="/clinic/admin"><ClinicWrapper admin_only="{true}" component="{AdminOverview}"/></Route>
-    <Route path="/clinic/login" component="{Login}"/>
-    <Route path="/clinic/login/complete_profile" component="{CompleteProfile}"/>
-    <Route path="/">
-        <Redirect to="/clinic/login"/>
+    <Route path='/clinic'><ClinicWrapper component='{AppointmentDashboard}'/></Route>
+    <Route path='/clinic/appointments'><ClinicWrapper component='{AppointmentOverview}'/></Route>
+    <Route path='/clinic/patients'><ClinicWrapper component='{PatientOverview}'/></Route>
+    <Route path='/clinic/admin'><ClinicWrapper admin_only='{true}' component='{AdminOverview}'/></Route>
+    <Route path='/clinic/login' component='{Login}'/>
+    <Route path='/clinic/login/complete_profile' component='{CompleteProfile}'/>
+    <Route path='/clinic/login/change_password' component='{ChangePassword}'/>
+    <Route path='/clinic/login/verify_email' component='{VerifyEmail}'/>
+    <Route path='/'>
+        <Redirect to='/clinic/login'/>
     </Route>
 </Router>
--- src/Components/Clinic/Splash/SplashButton.svelte
+++ src/Components/Clinic/Splash/SplashButton.svelte
@@ -4,14 +4,15 @@
     export let validationMessage = "";
     export let message = "BUTTON";
     export let icon = false;
+    export let disabled = false;
 </script>
-<div class="uk-margin-bottom uk-text-center">
-    <span class="uk-text-danger uk-text-emphasis">{validationMessage}</span>
+<div class="uk-margin-small-bottom uk-text-center">
+    <p class="uk-text-danger uk-text-emphasis uk-margin-small-bottom">{validationMessage}</p>
     <div class="uk-inline uk-width-1-1">
         {#if icon}
             <Icon {icon} _class="uk-form-icon uk-form-icon-flip"/>
         {/if}
-        <button type="button" class="uk-button uk-button-default uk-border-pill uk-width-1-1" on:click>
+        <button type="button" class="uk-button uk-button-default uk-border-pill uk-width-1-1" on:click {disabled} class:uk-disabled={disabled}>
             {message}
         </button>
     </div>
